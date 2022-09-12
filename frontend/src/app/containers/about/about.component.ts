c1ccced893af8efd3c42c4f45b16182e1f582e68
olha-bondarenko <olga.bondarenko.nl@gmail.com>
2022-09-12 19:52:29 +0200

    wip: add about-hobbies info

--- frontend/src/app/containers/about/about.component.ts
+++ frontend/src/app/containers/about/about.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   expDescription: string = 'Below is a short list of major dates and experiences in my career.';
-  skillDescription: string = 'The skillset I am proud of.';
+  skillDescription: string = 'The list of my hard and soft skills.';
+  hobbiesDescription: string = 'Things I am busy with when I am not coding.';
   ironhackUrl: string = 'https://www.ironhack.com/';
   edCastUrl: string = 'https://www.edcast.com/';
   constructor() { }
